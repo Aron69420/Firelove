240df0d4cadb0e869e797b6f206611c1c0196394
Aron69420 <143184699+Aron69420@users.noreply.github.com>
2023-11-05 20:16:13 +0100

    Add files via upload

---
+++ registrierung/emo/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace main_cs
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
